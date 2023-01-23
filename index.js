9f20b9643fdcbded159719104ce5817b521dac7c
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-01-23 17:02:54 +0300

    complete setting up

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-
+const Data=require ("./Mock_DATA.json");
+const graphq=require('graphql')
+const {graphqlHTTP}=require("express-graphql")
 app.get("/test", (req, res) => {
   res.send("hello there? My new graphql api");
 });
